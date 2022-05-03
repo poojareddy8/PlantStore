706ead65d98d7f588c3dd853762e88c1b1da54d9
poojareddy8 <104426968+poojareddy8@users.noreply.github.com>
2022-05-03 14:21:24 +0530

    Add files via upload

---
+++ OnlinePlantStore1/src/main/java/com/abc/OnlinePlantStore/OnlinePlantStore1Application.java
@@ -0,0 +1,13 @@
+package com.abc.OnlinePlantStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePlantStore1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePlantStore1Application.class, args);
+	}
+
+}
---
+++ OnlinePlantStore1/src/test/java/com/abc/OnlinePlantStore/OnlinePlantStore1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.OnlinePlantStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePlantStore1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
